377cd900d9b37a9a484e8f7e81d7958388d407fe
1Sat84 <juan.bulhos@gmail.com>
2023-11-13 18:35:18 -0300

    final palindromo e arrayminmax
    projeto pronto

--- palindromo/script.js
+++ palindromo/script.js
@@ -1,34 +1,32 @@
+//Palindrome
 function isPalindrome(str) {
-  str = str.replace(/\s/g, '').toLowerCase();
-  let start = 0;
-  let end = str.length - 1;
+str = str.replace(/\s/g, '').toLowerCase();
+let start = 0;
+let end = str.length - 1;
 
-  while (start < end) {
-  
-      if (str[start] !== str[end]) {
-          return false;
-      }
-      start++;
-      end--;
-  }
-  return true;
+ while (start < end) {
+ if (str[start] !== str[end]) {
+ return false;
+ }
+  start++;
+  end--;
+ }
+return true;
 }
 
-function arrayMaxMin(arr) {
+  //Minmax
+  function arrayMaxMin(arr) {
   if (arr.length === 0) {
       return [];
   }
-
   let max = arr[0];
   let min = arr[0];
-
   for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > max) {
-          max = arr[i];
-      } else if (arr[i] < min) {
-          min = arr[i];
-      }
+  if (arr[i] > max) {
+   max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
   }
-
   return [min, max];
 }
